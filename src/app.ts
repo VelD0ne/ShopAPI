0f0c6a0e8bef50a481059c7c3df6e3a0f2ab8352
VelD0ne <Volosevichdan@gmail.com>
2022-07-15 16:35:08 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config(); //Reads .env file and makes it accessible via process.env
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.send("hi");
+});
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello, World");
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at ${process.env.PORT}`);
+});
+
+import { Pool } from "pg";
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
+});
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
