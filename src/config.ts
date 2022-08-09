1ed20eb2ff7f9647e0d02f229c0b253eabbebe64
Daniil <Volosevichdan@gmail.com>
2022-08-09 17:20:42 +0300

    config file added

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+import session from "../types";
+
+const appSession = session({
+    secret: String(process.env.SESSION_SECRET),
+    resave: false,
+    saveUninitialized: true,
+});
+
+const serverPort = process.env.PORT || 5000;
+
+export { appSession, serverPort };
