a9b4ab875a5b0ffc7e09a86c4f30c916987f12e5
Daniil <Volosevichdan@gmail.com>
2022-08-09 17:22:54 +0300

    app file are refactored

--- src/app.ts
+++ src/app.ts
@@ -2,40 +2,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import session from "../types";
+import { appSession, serverPort } from "./config";
 import { router } from "./routes/index";
 import { myDataSource } from "./db/appDataSource";
 
 const start = async () => {
-    await myDataSource
-        .initialize()
-        .then(() => {
-            console.log("Data Source has been initialized!");
-        })
-        .catch((err: Error) => {
-            console.error("Error during Data Source initialization:", err);
-            return;
-        });
+    await myDataSource.initialize();
+    console.log("Data source has been initialized");
 
     const app = express();
 
     app.use(express.json());
 
-    app.use(
-        session({
-            secret: String(process.env.SESSION_SECRET),
-            resave: false,
-            saveUninitialized: true,
-        })
-    );
+    app.use(appSession);
 
     app.use(router);
 
-    const port = process.env.PORT || 5000;
-
-    app.listen(port, () => {
-        console.log(`Server is running at ${port}`);
+    app.listen(serverPort, () => {
+        console.log(`Server is running at ${serverPort}`);
     });
 }
 
-start();
+start().catch(console.error);
